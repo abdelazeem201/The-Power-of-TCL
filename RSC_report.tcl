bc57fad9194b9e428417ba01a3ca36182bf442bb
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2023-01-06 22:28:33 +0200

    Create RSC_report.tcl

---
+++ RSC_report.tcl
@@ -0,0 +1,7 @@
+edirect clock_sink.rpt {
+    foreach_in_collection ck [all_clocks] {
+        puts "Clock_Name No_of_register_sinks Sink_list"
+        puts "#####################################"
+    puts "[get_object_name $ck] [sizeof_collection [all_registers -clock $ck]] [get_object_name [all_registers -clock $ck]]\n"
+    }
+}
